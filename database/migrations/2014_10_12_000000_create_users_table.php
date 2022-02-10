7de33d4ee3e4dcab728b99e9ca20d20f0bbf6a31
phreneia <Phreneia@DESKTOP-CRJPKT3>
2022-02-10 10:05:36 +0700

    fixing error

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -23,6 +23,8 @@ class CreateUsersTable extends Migration
             $table->foreignId('current_team_id')->nullable();
             $table->string('profile_photo_path', 2048)->nullable();
             $table->timestamps();
+            $table->unsignedBigInteger('role_id')->nullable()->unsigned();
+            $table->foreign('role_id')->references('id')->on('roles')->onDelete('cascade');
         });
     }
 
